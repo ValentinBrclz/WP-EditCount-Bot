657fbf6a5c4fc3aeaec0f9d7261ae585feb53c25
Valentin Berclaz <ValentinBrclz@users.noreply.github.com>
2017-08-07 15:28:45 +0200

    Adapt `run.sh` to new environment

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+# Go to DIR
+cd ~/WP-EditCount-Bot
+
 # Get the last version
 git pull
 
-# Install any new package
+# Install any new package (prod-only)
 npm install --production
 
-# Start with forever
+# Start
 /shared/bin/node lib/bot.js
